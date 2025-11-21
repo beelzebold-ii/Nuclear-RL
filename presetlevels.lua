3224a238e5d8f94d3f718052ec1beb731b3fad98
beelzebold-ii <futurefast125@gmail.com>
2025-11-21 17:37:05 -0600

    support for custom levels is now complete probably

--- presetlevels.lua
+++ presetlevels.lua
@@ -3,32 +3,39 @@
 presetlevels = {
 	[4][1] = {
 		name = "Security Checkpoint",
-		entrytext = "Entering the checkpoint. What a nightmare this place is.",
+		filename = "checkpoint.json",
+		entrytext = "Entering the checkpoint. High security, what hospitality.",
+		calmtext = "All is calm. But my documents were all in order!",
 		music = love.audio.newSource("music/Meltdown.wav","stream"),
 	},
 	[8][1] = {
 		name = "Waste Refinery Center",
 		entrytext = "Entering the refinery. Do I have to go down there?",
+		calmtext = "All is calm. Now I gotta go before my lungs collapse.",
 		music = love.audio.newSource("music/LongWar.wav","stream"),
 	},
 	[8][2] = {
 		name = "Barricade",
-		entrytext = "Some kind of barricade. It seems they came prepared.",
+		entrytext = "Some kind of barricade. It seems they were waiting for me.",
+		calmtext = "All is calm. What a nightmare.",
 		music = love.audio.newSource("music/LongWar.wav","stream"),
 	},
 	[12][1] = {
 		name = "Medical Room",
 		entrytext = "Entering the medical room. I sense a certain tension.",
+		calmtext = "All is calm. The scent of viscera is overwhelming.",
 		music = love.audio.newSource("music/LongWar.wav","stream"),
 	},
 	[12][2] = {
 		name = "High Security Vault",
 		entrytext = "Entering the vault. It's like a fortress down here.",
+		calmtext = "All is calm. Now what kind of loot did you leave me?",
 		music = love.audio.newSource("music/LongWar.wav","stream"),
 	},
 	[12][3] = {
 		name = "Drone Swarm",
 		entrytext = "Entering the main lab. What's that sound...? Uh oh.",
+		calmtext = "All is calm. Now where can I lay down to die in peace?",
 		music = love.audio.newSource("music/LongWar.wav","stream"),
 	},
 }
